391220fb9059d74190885b58e051f9bda3cde280
dmmirtiaws <70662152+dmmirtiaws@users.noreply.github.com>
2020-09-06 15:48:00 -0700

    Create testfile2.h

---
+++ src/testfile2.h
@@ -0,0 +1,23 @@
+/**
+ * @file
+ * @brief
+ * This file declares the public API data types, structures and functions that comprise the Test File 2.
+ */
+
+// Doxygen for Test File 2
+/** @page test_file_page2 Test File 2 Home Page
+ * @tableofcontents
+ *
+ * @section test_file2_arch Test File 2 Architecture
+ */
+ 
+ 
+/**
+ * Do this function and be happy forever.
+ *
+ * @param enable Use true to enable, false to disable.
+ * @param level Happiness level to enable.
+ *
+ * @return A value from 1 to 10 of how happy you'll be.
+ */
+int MakeMeHappy2(bool enable, int level);
