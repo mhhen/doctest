c2d004fad8de827799ea1d17fdf1f0e780559021
dmmirtiaws <70662152+dmmirtiaws@users.noreply.github.com>
2020-09-08 15:19:48 -0700

    Create versions.h

---
+++ src/versions.h
@@ -0,0 +1,8 @@
+/// @brief SDK version.
+#define SDK_VERSION             1
+
+/// @brief SDK major version.
+#define SDK_MAJOR_VERSION       0
+
+/// @brief SDK minor version.
+#define SDK_MINOR_VERSION       0
